a3bacf4c14fec60c0f9c9750b93ff6a778b4dbfd
jmcwhirter1608 <78233755+jmcwhirter1608@users.noreply.github.com>
2023-04-17 16:18:23 -0500

    Updating index.php with comment for navbar

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Cooking Mania</title>
 </head>
 <body>
-    <?php include "navbar.php"?>
+    <?php include "navbar.php"?> <!-- Include This for navbar /!-->
     <h1>Welcome to Cooking Mania</h1>
     <?php 
     echo "Hello World, Ariana Pouya is a nerd! :D"
