8c0379e2c0520833897af242fe2daec5c76a8142
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-04-23 13:32:12 -0500

    Updated Sign In Page
    Can Completely Sign In to an account.
    TODO: Link Pages to save user or save user password and such to link pages

--- SignIn.php
+++ SignIn.php
@@ -4,19 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\styles.css">
+
     <title>Sign In</title>
 </head>
 <body>
     <?php include 'navbar.php'?>
+    <?php include 'dbconnection.php'?>
+
     <?php
         // define variables and set to empty values
-        $uname = $email = $psw = $psw_repeat = "";
+        $email = $psw = $psw_repeat = "";
+        $unameErr = $emailErr = $pswErr = $psw_repeatErr ='';
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $uname = test_input($_POST["uname"]);
-        $email = test_input($_POST["email"]);
-        $psw = test_input($_POST["psw"]);
-        $psw_repeat = test_input($_POST["psw-repeat"]);
+
+            $email = test_input($_POST["email"]);
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $emailErr = "Invalid Email";
+
+            }
+            $psw = test_input($_POST["psw"]);
+            $psw_repeat = test_input($_POST["psw-repeat"]);
+            if($psw != $psw_repeat)
+                $pswErr = "Passwords are not equal";
+            else
+                $pswErr = '';
         }
 
         function test_input($data) {
@@ -30,32 +43,48 @@
   <div class="register">
     <h1>Sign In</h1>
 
-    <label for="lname"><b>User Name</b></label>
-    <input type="text" placeholder="Enter User Name" name="uname" required><br/>
-
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required><br/>
+    <input type="text" placeholder="Enter Email" name="email" value="<?php echo $email;?>" required><br/>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required><br/>
+    <input type="password" placeholder="Enter Password" name="psw" value="<?php echo $psw;?>" required><br/>
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required><br/>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" value="<?php echo $psw_repeat;?>" required><br/>
 
     <br>
     <input type='submit' name='submit' value='Sign In'>
   </div>
-</form> 
+</form>
     <?php
-    echo "<h2>Your Input:</h2>";
-    echo $uname;
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo $psw;
-    echo "<br>";
-    echo $psw_repeat;
+    if(empty($email))
+        echo "Error: $emailErr <br>";
+    else
+        if(empty($pswErr)){
+            $sql = "SELECT * from users WHERE User_email = \"$email\"";
+            $result = $connection->query($sql);
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    if($row['User_password'] == $psw){
+                        echo "Welcome back " . $row['User_fname'] . " " . $row['User_lname'] ."<br>";
+                    } else {
+                        echo "Incorrect Email or Password";
+                    }
+                } 
+            } else {
+                echo "Incorrect Email or Password";
+            }
+        }
+        else
+            echo "Error: $pswErr <br>";
+
+
+    if ($connection->connect_error){
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+
+    
     ?>
 </body>
 </html>
-
