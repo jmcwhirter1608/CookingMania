021927e8dde4e80d95210b2cef635498e0f1e345
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-04-29 23:21:20 -0500

    Updated SQL to include CASCADE
    CASCADE allows a user to be deleted accross the Database.

--- Accounts.php
+++ Accounts.php
@@ -34,6 +34,18 @@
     </div>
     <?php include 'Register/EditAccount.php'?>
     <h1>Delete</h1>
+    <div id='DeleteAccount-RowForm'>
+        <div id='DeleteAccount-RowForm-Row-header'>User ID</div>
+        <div id='DeleteAccount-RowForm-Row-header'>User Type</div>
+        <div id='DeleteAccount-RowForm-Row-header'>First Name</div>
+        <div id='DeleteAccount-RowForm-Row-header'>Last Name</div>
+        <div id='DeleteAccount-RowForm-Row-header'>Email</div>
+        <div id='DeleteAccount-RowForm-Row-header'>Phone Number</div>
+        <div id='DeleteAccount-RowForm-Row-header'>Password</div>
+        <div id='DeleteAccount-RowForm-Row-Delete-header'>To Delete</div>
+    </div>
+    <?php include 'Register/DeleteAccount.php'?>
+
 
 
     
---
+++ Register/DeleteAccount.php
@@ -0,0 +1,116 @@
+<?php
+
+    $acc_type = $uid = $fname = $lname = $email = $phone = $psw = NULL;
+
+    $sql = sprintf("SELECT * FROM users");
+
+    try{
+        echo "<form id='DeleteAccount-RowForm' method='post' action=". htmlspecialchars($_SERVER["PHP_SELF"]) . ">";
+        $result = $connection->query($sql);
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                echo   "<div id='DeleteAccount-RowForm-Row' >" . $row['User_ID'] . "</div>
+                        <div id='DeleteAccount-RowForm-Row' >". $row['User_type'] . "</div>
+                        <div id='DeleteAccount-RowForm-Row' >" . $row['User_fname'] . "</div>
+                        <div id='DeleteAccount-RowForm-Row' >" . $row['User_lname'] ."</div>
+                        <div id='DeleteAccount-RowForm-Row' >" . $row['User_email'] . "</div>
+                        <div id='DeleteAccount-RowForm-Row' >" . $row['User_phonenumber'] . "</div>
+                        <div id='DeleteAccount-RowForm-Row' >" . $row['User_password'] . "</div>
+                        <input id='DeleteAccount-RowForm-Row-Delete' type='checkbox' name='check[" . $row['User_ID'] . "]' >
+                        ";
+                
+            }
+            echo "<input type='submit' name='Delete-submit' value='Delete'></form>";
+        }
+    } catch(Exception $e){
+        echo "Error: " . $e->getMessage();
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Delete-submit'])) {
+
+        
+        $postKeys = array_keys($_POST['check']);
+
+        $sql_ids = '(';
+        $last = end($postKeys);
+        foreach($_POST['check'] as $id => $value){
+            if($id == $last){
+                $sql_ids .= $id .')';
+            } else{
+                $sql_ids .= $id . ',';
+            }
+        }
+
+        $sql = sprintf("DELETE FROM class_enrollment
+        WHERE User_ID IN %s;
+        ",
+        $sql_ids
+        );
+        
+        try{
+            $result = $connection->query($sql);
+            if($result === TRUE){
+                echo "Successfully deleted Class Enrollments<br>";
+            }
+        } catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+        }
+
+        $sql = sprintf("DELETE FROM comments
+        WHERE User_ID IN %s;
+        ",
+        $sql_ids);
+
+        try{
+            $result = $connection->query($sql);
+            if($result === TRUE){
+                echo "Successfully deleted comments<br>";
+            }
+        } catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+        }
+
+        $sql = sprintf("DELETE FROM recipes
+        WHERE User_ID IN %s;
+        ",
+        $sql_ids);
+
+        try{
+            $result = $connection->query($sql);
+            if($result === TRUE){
+                echo "Successfully deleted recipes<br>";
+            }
+        } catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+        }
+
+        $sql = sprintf("DELETE FROM classes
+        WHERE User_ID IN %s;
+        ",
+        $sql_ids);
+
+        try{
+            $result = $connection->query($sql);
+            if($result === TRUE){
+                echo "Successfully deleted classes<br>";
+            }
+        } catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+        }
+
+        $sql = sprintf("DELETE FROM users
+        WHERE User_ID IN %s;
+        ",
+        $sql_ids);
+
+        try{
+            $result = $connection->query($sql);
+            if($result === TRUE){
+                echo "Successfully deleted user<br>";
+            }
+        } catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+        }
+        
+    }
+?>
