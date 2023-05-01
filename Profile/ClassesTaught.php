acc2c30fa344d3a1ff66cda91637ffc1d9945ec5
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-05-01 15:11:12 -0500

    Update ClassesTaught.php
    Added a Classes Taught table with Date, Time, Recipe name, Start Time, Duration, and class attendance

--- Profile/ClassesTaught.php
+++ Profile/ClassesTaught.php
@@ -7,33 +7,40 @@
    
 
     $sql = sprintf(
-        "SELECT class_enrollment.Class_ID, classes.Class_Date, classes.Class_StartTime, recipes.Recipe_name, users.User_fname, users.User_lname, classes.Class_duration, classes.Class_RoomNum
-        FROM class_enrollment
-        INNER JOIN classes
-        ON class_enrollment.Class_ID = classes.Class_ID
+        "SELECT
+            classes.Class_Date,
+            classes.Class_StartTime,
+            recipes.Recipe_name,
+            classes.Class_duration,
+            classes.Class_RoomNum,
+            s.mycount AS Class_Attendance
+        FROM classes
         INNER JOIN recipes
-        ON classes.Recipe_ID = recipes.Recipe_ID
-        INNER JOIN users
-        ON classes.User_ID = users.User_ID
-        WHERE class_enrollment.User_ID = %d;",
+        ON recipes.Recipe_ID = classes.Recipe_ID
+        INNER JOIN (
+            SELECT 
+                class_enrollment.Class_ID as Class_ID,
+                COUNT(class_enrollment.User_ID) as mycount
+            FROM class_enrollment
+            GROUP BY class_enrollment.Class_ID) s
+        ON s.Class_ID = classes.Class_ID
+        WHERE classes.User_ID = %d;",
         $_SESSION["User_ID"]
     );
 
     $result = $connection->query($sql);
 
     if($result->num_rows > 0){
-        echo "<table><tr><th>Date</th><th>Start Time</th><th>Recipe</th><th>Teacher</th><th>Duration</th><th>Room Number</th></tr>";
+        echo "<table><tr><th>Date</th><th>Start Time</th><th>Recipe</th><th>Duration</th><th>Room Number</th><th>Attendance</tr>";
         while($row = $result->fetch_assoc()){
             echo "<tr>
                     <td>" . $row['Class_Date'] . "</td>
                     <td>" . $row['Class_StartTime'] . "</td>
                     <td>" . $row['Recipe_name'] . "</td>
-                    <td>" . $row['User_fname'] . " " . $row['User_lname'] . "</td>
                     <td>" . $row['Class_duration'] . "</td>
                     <td>" . $row['Class_RoomNum'] . "</td>
-                   </tr>";
-            
+                    <td>" . $row['Class_Attendance'] . "</td>
+                    </tr>";
         }
     }
-
 ?>
