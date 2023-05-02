8e96f6ab1a3248020bb3d1f856a4ac4cd69dd3da
Ari-Pouya <123524057+Ari-Pouya@users.noreply.github.com>
2023-05-02 12:17:51 -0500

    create comment function

--- Ariana.php
+++ Ariana.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ariana</title>
+    <link rel="stylesheet" href="css\styles.css">
+    <title>Comment</title>
 </head>
 
 
@@ -17,19 +18,43 @@
     <?php include "dbconnection.php"?>
 
    <?php 
-   // get the recipes name from the recipe table
-   $sql = sprintf("SELECT Recipe_name FROM recipes");
+   // sql statement to get the recipes from the recipe table
+   $sql = sprintf("SELECT * FROM recipes"); 
    $result = $connection->query($sql);
 
-   while ( $recipe = $result->fetch_assoc()) {
+   //print out recipes name 
+   while ( $row = $result->fetch_assoc()) {
+
+    // get the comment from database
+     $sql1 = sprintf("SELECT * 
+     FROM comments
+     INNER JOIN users
+     ON users.User_ID = comments.User_ID
+     WHERE Recipe_ID = %d", $row['Recipe_ID']);
+
+     $result1 = $connection->query($sql1);
+
+
+
       Print '<hr />';
-      Print '<h2 align="center">'. $recipe['Recipe_name'] . "</h2>"; 
+      Print '<h2 align="center">'. $row['Recipe_name'] . "</h2>"; 
+      Print '<h2 align="center"> Comments </h2> '; 
+
+      while ( $row1 = $result1->fetch_assoc()){
+        Print '<h2 align="center">'. $row1['User_fname'] . ": " . $row1['CommentText'] . "</h2>"; 
+      };
+
+      //create comment form, INSERT INTO Comments(Comment_ID, Recipe_ID,User_ID, CommentText)
+      Print '<form method = "post" action = "CreateSQLComment.php?Recipe_ID='.$row['Recipe_ID'].'">
+          <label> Comment: </label> <input type = "text" name = "comment"> <br>
+         <input type="submit" name="Create-comment" value="Submit">
+      </form>';
    }
-
-
-
-    ?>
+?>
   
-    
+
+
+
+
 </body>
 </html>
