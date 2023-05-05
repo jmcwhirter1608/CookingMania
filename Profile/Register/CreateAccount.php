5af148eeee7faa31aa966f249fabb9c8bb63c848
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-05-05 01:46:40 -0500

    Updating bugs

--- Profile/Register/CreateAccount.php
+++ Profile/Register/CreateAccount.php
@@ -33,41 +33,98 @@
 
     </form> 
     <?php
-        if(isset($_POST['Create-submit'])){
-            if(empty($fname) || empty($lname) || empty($email) || empty($phone) || empty($psw) || empty($psw_repeat) || empty($acc_type)){
-            if($Err != NULL)
-                echo "Error: ". $Err . "<br>";
-            }
-            else{
-                $sql = sprintf("INSERT INTO `users` (`User_ID`, `User_type`, `User_fname`, `User_lname`, `User_email`, `User_phonenumber`, `User_password`)
-                VALUES (NULL, %d , '%s', '%s', '%s', %d, '%s')", 
-                $acc_type,
-                $connection->real_escape_string($fname),
-                $connection->real_escape_string($lname),
-                $connection->real_escape_string($email),
-                $connection->real_escape_string($phone),
-                $connection->real_escape_string($psw));
-                try {
-                    $result = $connection->query($sql);
-                    
-                    if($result === TRUE){
-                        echo "Account Created, Welcome $fname $lname!";
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Create-submit'])){
+            switch(0){
+                case 0: 
+                    if(empty($_POST['fname'])){
+                        $Err = "First Name is Required";
+                        break;
+                    } else{
+                        $fname = test_input($_POST['fname']);
                     }
-                } catch(Exception $e) {
-                    switch($e->getCode()){
-                    case 1062:
-                        echo "Error: Email or Phone Number in use <br>";
-                    default:
-                        echo "Error: " . $e->getMessage();
+                case 1:
+                    if(empty($_POST['lname'])){
+                        $Err = "Last Name is Required";
+                        break;
+                    } else{
+                        $lname = test_input($_POST['lname']);
+                    }
+                case 2:
+                    if(empty($_POST['email'])){
+                        $Err = "Email is Required";
+                        break;
+                    } else{
+                        $email = test_input($_POST['email']);
+                    }
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                        $Err = "Invalid Email";
+                        break;
+                    }
+                    $query = sprintf("SELECT * FROM users 
+                    WHERE User_email='%s' AND NOT User_ID=%d",
+                    $connection->real_escape_string($email),
+                    $_SESSION['User_ID']);
+                    $result = $connection->query($query);
+                    if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){
+                            $Err = 'Email already in use: ' . $row['User_email'] . ' for User ID ' . $row['User_ID'];
+                        }
+                        break;
+                    }
+                case 3:
+                    if(empty($_POST['phone'])){
+                        $Err = "Phone is Required";
+                        break;
+                    } else{
+                        $phone = test_input($_POST['phone']);
+                    }
+                    if(preg_match('/^[0-9]{10}+$/', $phone) == FALSE){
+                        $Err = "Invalid Phone Number (Format: 1234567890)";
+                        break;
+                    }
+                case 4:
+                    if(empty($_POST['psw'])){
+                        $Err = "Password is Required";
+                        break;
+                    } else{
+                        $psw = test_input($_POST['psw']);
+                    }
+                case 5:
+                    if(empty($_POST['psw-repeat'])){
+                        $Err = "Repeat Password is Required";
+                        break;
+                    } else{
+                        $psw_repeat = test_input($_POST['psw-repeat']);
+                        if($psw != $psw_repeat){
+                            $Err = "Passwords Do not Match";
+                            break;
+                        }
+                    }
+                case 6:
+                    $sql = sprintf("INSERT INTO users
+                    (`User_ID`, `User_type`, `User_fname`, `User_lname`, `User_email`, `User_phonenumber`, `User_password`)
+                    VALUES (NULL, %d , '%s', '%s', '%s', %d, '%s')", 
+                    $_POST['acc-type'],
+                    $connection->real_escape_string($fname),
+                    $connection->real_escape_string($lname),
+                    $connection->real_escape_string($email),
+                    $connection->real_escape_string($phone),
+                    $connection->real_escape_string($psw));
+                    try {
+                        $result = $connection->query($sql);
+                        
+                        if($result === TRUE){
+                            echo "New Account Created for $fname $lname!";
+                        }
+                    } catch(Exception $e) {
+                        switch($e->getCode()){
+                        case 1062:
+                            echo "Error: Email or Phone Number in use <br>";
+                        default:
+                            echo "Error: " . $e->getMessage();
+                        }
                     }
                 }
+
             }
-
-            if ($connection->connect_error){
-                die("Connection failed: " . $connection->connect_error);
-            }
-        }   
-
-
-        
         ?>
