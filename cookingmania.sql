c719066cce7890a54e5a9dec787647f54cd525b1
JohnMcWhirter10 <jmcwhirter1608@tamu.edu>
2023-05-05 11:52:08 -0500

    Update cookingmania.sql
    Updating sql with Indexes and Views for users

--- cookingmania.sql
+++ cookingmania.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2023 at 12:24 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 05, 2023 at 06:51 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `cookingmania`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `admins`
+-- (See below for the actual view)
+--
+CREATE TABLE `admins` (
+`User_ID` int(11)
+,`User_type` int(11)
+,`User_fname` varchar(255)
+,`User_lname` varchar(255)
+,`User_email` varchar(255)
+,`User_phonenumber` varchar(255)
+,`User_password` varchar(255)
+);
 
 -- --------------------------------------------------------
 
@@ -93,16 +109,48 @@ CREATE TABLE `recipes` (
   `Recipe_instructions` mediumtext DEFAULT NULL,
   `Recipe_Ingredients` text NOT NULL,
   `last_update_date` date DEFAULT NULL,
-  `User_ID` int(11) DEFAULT NULL,
+  `User_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `recipes`
 --
 
-INSERT INTO `recipes` (`Recipe_ID`, `Recipe_name`, `Recipe_time`, `Recipe_level`, `Recipe_instructions`, `last_update_date`, `User_ID`, `Recipe_Ingredients`) VALUES
-(1, 'Pizza', '30', 1, 'Roll Dough, Add Sauce, Add Cheese, Cook for 25', '2023-05-01', 19, ''),
-(2, 'Chicken Parmesan', '45', 2, 'Cook Chicken Till Burnt\r\nPlace marinara in air fryer\r\nStir up the sauce\r\nRun it with the crew', '2023-05-01', 19, '');
+INSERT INTO `recipes` (`Recipe_ID`, `Recipe_name`, `Recipe_time`, `Recipe_level`, `Recipe_instructions`, `Recipe_Ingredients`, `last_update_date`, `User_ID`) VALUES
+(1, 'Pizza', '30', 1, 'Roll Dough, Add Sauce, Add Cheese, Cook for 25', '', '2023-05-01', 19),
+(2, 'Chicken Parmesan', '45', 2, 'Cook Chicken Till Burnt\r\nPlace marinara in air fryer\r\nStir up the sauce\r\nRun it with the crew', '', '2023-05-01', 19);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `students`
+-- (See below for the actual view)
+--
+CREATE TABLE `students` (
+`User_ID` int(11)
+,`User_type` int(11)
+,`User_fname` varchar(255)
+,`User_lname` varchar(255)
+,`User_email` varchar(255)
+,`User_phonenumber` varchar(255)
+,`User_password` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `teachers`
+-- (See below for the actual view)
+--
+CREATE TABLE `teachers` (
+`User_ID` int(11)
+,`User_type` int(11)
+,`User_fname` varchar(255)
+,`User_lname` varchar(255)
+,`User_email` varchar(255)
+,`User_phonenumber` varchar(255)
+,`User_password` varchar(255)
+);
 
 -- --------------------------------------------------------
 
@@ -133,6 +181,33 @@ INSERT INTO `users` (`User_ID`, `User_type`, `User_fname`, `User_lname`, `User_e
 (16, 3, 'Harry', 'Potter', 'thechosenone@gmail.com', '1234567890', 'slytherin'),
 (18, 3, 'Hermione', 'Granger', 'smartgirl@gmail.com', '1234567890', 'asdf'),
 (19, 2, 'Severus', 'Snape', 'HalfBlodP@gmail.com', '1234567890', 'asdf');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `admins`
+--
+DROP TABLE IF EXISTS `admins`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `admins`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `students`
+--
+DROP TABLE IF EXISTS `students`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `students`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `teachers`
+--
+DROP TABLE IF EXISTS `teachers`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `teachers`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 2 ;
 
 --
 -- Indexes for dumped tables
@@ -168,14 +243,16 @@ ALTER TABLE `comments`
 --
 ALTER TABLE `recipes`
   ADD PRIMARY KEY (`Recipe_ID`),
-  ADD UNIQUE KEY `Recipe_ID` (`Recipe_ID`);
+  ADD UNIQUE KEY `Recipe_ID` (`Recipe_ID`),
   ADD KEY `Recipe_User` (`User_ID`);
+
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`User_ID`),
-  ADD UNIQUE KEY `User_ID` (`User_ID`);
+  ADD UNIQUE KEY `User_ID` (`User_ID`),
+  ADD KEY `User_type` (`User_type`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -229,10 +306,12 @@ ALTER TABLE `class_enrollment`
 ALTER TABLE `comments`
   ADD CONSTRAINT `Comment_Recipe` FOREIGN KEY (`Recipe_ID`) REFERENCES `recipes` (`Recipe_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Comment_user` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
+--
+-- Constraints for table `recipes`
+--
 ALTER TABLE `recipes`
-  ADD CONSTRAINT `Recipe_User` FOREIGN KEY (`User_ID`) REFERENCES `Users` (`User_ID`);
+  ADD CONSTRAINT `Recipe_User` FOREIGN KEY (`User_ID`) REFERENCES `users` (`User_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
