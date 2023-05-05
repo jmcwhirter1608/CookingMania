fd424eebc34a04feb18647947d164c4ec5ce5b6d
ShriMathavan <shriam02@gmail.com>
2023-05-05 13:06:28 -0500

    Added views and indexes for classes

--- cookingmania.sql
+++ cookingmania.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 05, 2023 at 06:51 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost
+-- Generation Time: May 05, 2023 at 08:03 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -50,16 +50,40 @@ CREATE TABLE `classes` (
   `Class_Date` date NOT NULL,
   `Class_StartTime` time NOT NULL,
   `Class_RoomNum` int(11) NOT NULL,
-  `Class_EndTime` int(11) NOT NULL
+  `Class_EndTime` time NOT NULL,
+  `Class_Enrollment` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `classes`
 --
 
-INSERT INTO `classes` (`Class_ID`, `Recipe_ID`, `User_ID`, `Class_Date`, `Class_StartTime`, `Class_RoomNum`, `Class_EndTime`) VALUES
-(1, 1, 19, '2023-05-01', '12:14:30', 201, 0),
-(2, 2, 19, '2023-05-02', '12:14:30', 201, 0);
+INSERT INTO `classes` (`Class_ID`, `Recipe_ID`, `User_ID`, `Class_Date`, `Class_StartTime`, `Class_RoomNum`, `Class_EndTime`, `Class_Enrollment`) VALUES
+(1, 1, 19, '2023-05-16', '09:00:00', 201, '10:00:00', 13),
+(2, 1, 19, '2023-05-22', '10:30:00', 201, '11:00:00', 16),
+(3, 2, NULL, '2023-05-22', '15:10:00', 432, '16:10:00', 20),
+(4, 1, NULL, '2023-05-17', '09:30:00', 312, '10:10:00', 44),
+(5, 2, 7, '2023-05-20', '12:00:00', 432, '12:40:00', 24);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `classes_recipes_matching`
+-- (See below for the actual view)
+--
+CREATE TABLE `classes_recipes_matching` (
+`User_ID` int(11)
+,`Recipe_name` varchar(255)
+,`Class_Date` date
+,`Class_StartTime` time
+,`Class_EndTime` time
+,`Class_RoomNum` int(11)
+,`Class_Enrollment` int(3)
+,`Class_ID` int(11)
+,`Recipe_time` varchar(255)
+,`Recipe_level` int(11)
+,`Recipe_instructions` mediumtext
+);
 
 -- --------------------------------------------------------
 
@@ -80,7 +104,8 @@ INSERT INTO `class_enrollment` (`Class_ID`, `User_ID`) VALUES
 (1, 16),
 (1, 18),
 (2, 15),
-(2, 16);
+(2, 16),
+(3, 18);
 
 -- --------------------------------------------------------
 
@@ -119,6 +144,23 @@ CREATE TABLE `recipes` (
 INSERT INTO `recipes` (`Recipe_ID`, `Recipe_name`, `Recipe_time`, `Recipe_level`, `Recipe_instructions`, `Recipe_Ingredients`, `last_update_date`, `User_ID`) VALUES
 (1, 'Pizza', '30', 1, 'Roll Dough, Add Sauce, Add Cheese, Cook for 25', '', '2023-05-01', 19),
 (2, 'Chicken Parmesan', '45', 2, 'Cook Chicken Till Burnt\r\nPlace marinara in air fryer\r\nStir up the sauce\r\nRun it with the crew', '', '2023-05-01', 19);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `registration_to_classes`
+-- (See below for the actual view)
+--
+CREATE TABLE `registration_to_classes` (
+`Class_ID` int(11)
+,`User_ID` int(11)
+,`Class_Date` date
+,`Class_RoomNum` int(11)
+,`Class_StartTime` time
+,`Class_EndTime` time
+,`Recipe_ID` int(11)
+,`Class_Enrollment` int(3)
+);
 
 -- --------------------------------------------------------
 
@@ -189,7 +231,25 @@ INSERT INTO `users` (`User_ID`, `User_type`, `User_fname`, `User_lname`, `User_e
 --
 DROP TABLE IF EXISTS `admins`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `admins`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 1 ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `admins`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 11  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `classes_recipes_matching`
+--
+DROP TABLE IF EXISTS `classes_recipes_matching`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `classes_recipes_matching`  AS SELECT `classes`.`User_ID` AS `User_ID`, `recipes`.`Recipe_name` AS `Recipe_name`, `classes`.`Class_Date` AS `Class_Date`, `classes`.`Class_StartTime` AS `Class_StartTime`, `classes`.`Class_EndTime` AS `Class_EndTime`, `classes`.`Class_RoomNum` AS `Class_RoomNum`, `classes`.`Class_Enrollment` AS `Class_Enrollment`, `classes`.`Class_ID` AS `Class_ID`, `recipes`.`Recipe_time` AS `Recipe_time`, `recipes`.`Recipe_level` AS `Recipe_level`, `recipes`.`Recipe_instructions` AS `Recipe_instructions` FROM (`classes` join `recipes` on(`classes`.`Recipe_ID` = `recipes`.`Recipe_ID`))  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `registration_to_classes`
+--
+DROP TABLE IF EXISTS `registration_to_classes`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `registration_to_classes`  AS SELECT `classes`.`Class_ID` AS `Class_ID`, `class_enrollment`.`User_ID` AS `User_ID`, `classes`.`Class_Date` AS `Class_Date`, `classes`.`Class_RoomNum` AS `Class_RoomNum`, `classes`.`Class_StartTime` AS `Class_StartTime`, `classes`.`Class_EndTime` AS `Class_EndTime`, `classes`.`Recipe_ID` AS `Recipe_ID`, `classes`.`Class_Enrollment` AS `Class_Enrollment` FROM (`classes` join `class_enrollment` on(`classes`.`Class_ID` = `class_enrollment`.`Class_ID`))  ;
 
 -- --------------------------------------------------------
 
@@ -198,7 +258,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `students`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `students`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 3 ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `students`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 33  ;
 
 -- --------------------------------------------------------
 
@@ -207,7 +267,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `teachers`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `teachers`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 2 ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `teachers`  AS SELECT `users`.`User_ID` AS `User_ID`, `users`.`User_type` AS `User_type`, `users`.`User_fname` AS `User_fname`, `users`.`User_lname` AS `User_lname`, `users`.`User_email` AS `User_email`, `users`.`User_phonenumber` AS `User_phonenumber`, `users`.`User_password` AS `User_password` FROM `users` WHERE `users`.`User_type` = 22  ;
 
 --
 -- Indexes for dumped tables
@@ -220,13 +280,15 @@ ALTER TABLE `classes`
   ADD PRIMARY KEY (`Class_ID`),
   ADD UNIQUE KEY `Class_ID` (`Class_ID`),
   ADD KEY `Class_User` (`User_ID`),
-  ADD KEY `Class_recipe` (`Recipe_ID`);
+  ADD KEY `Class_recipe` (`Recipe_ID`),
+  ADD KEY `Class_RoomNum` (`Class_RoomNum`);
 
 --
 -- Indexes for table `class_enrollment`
 --
 ALTER TABLE `class_enrollment`
   ADD PRIMARY KEY (`Class_ID`,`User_ID`),
+  ADD UNIQUE KEY `Class_ID` (`Class_ID`,`User_ID`),
   ADD KEY `Enrolled_User` (`User_ID`);
 
 --
@@ -262,7 +324,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `Class_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Class_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `comments`
